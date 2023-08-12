7cfc6039a167d1106ced45c59127c6543b91658b
Omid Bazangani <omid.bazangani@gmail.com>
2023-08-12 10:24:49 +0200

    solve the frequency update

--- ARM_Cortex-M4/STM32f407VE/LED_Blinking/LED_Blinking/src/main.c
+++ ARM_Cortex-M4/STM32f407VE/LED_Blinking/LED_Blinking/src/main.c
@@ -1,33 +1,67 @@
 #include "stm32f4xx.h"
 
-int main(void)
-{
-    // Set system clock to 168 MHz
-    SystemInit();
+void SystemClock_Config(void) {
+    // Enable HSE (High-Speed External clock)
+    RCC->CR |= RCC_CR_HSEON;
 
-    // Enable clock for GPIOE
+    // Wait until HSE is ready
+    while (!(RCC->CR & RCC_CR_HSERDY));
+
+    // Configure the PLL factors for HSE and set the system clock source 
+    RCC->PLLCFGR = (RCC_PLLCFGR_PLLSRC_HSE |      // HSE as PLL source
+                    25U |                         // PLLM = 25 for 25MHz HSE
+                    (336U << 6U) |                // PLLN = 336
+                    (0U << 16U) |                 // PLLP = 2 (00: PLLP = 2, 01: PLLP = 4, ...)
+                    (7U << 24U));                 // PLLQ = 7
+
+    // Enable the PLL
+    RCC->CR |= RCC_CR_PLLON;
+
+    // Wait until PLL is ready
+    while (!(RCC->CR & RCC_CR_PLLRDY));
+
+    // Set flash latency for 168MHz system clock
+    FLASH->ACR = FLASH_ACR_LATENCY_5WS;
+
+    // Select PLL as system clock source
+    RCC->CFGR |= RCC_CFGR_SW_PLL;
+
+    // Wait until PLL is selected as system clock source
+    while ((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_PLL);
+
+    // AHB prescaler (HCLK) set to 1
+    RCC->CFGR |= RCC_CFGR_HPRE_DIV1;
+
+    // APB1 prescaler set to 4 (max frequency 42MHz)
+    RCC->CFGR |= RCC_CFGR_PPRE1_DIV4;
+
+    // APB2 prescaler set to 2 (max frequency 84MHz)
+    RCC->CFGR |= RCC_CFGR_PPRE2_DIV2;
+}
+
+int main(void) {
+    // Initialize system clock
+    SystemClock_Config();
+
+    // Enable GPIO E clock
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIOEEN;
 
-    // Set mode of pin 13 to general purpose output
-    GPIOE->MODER &= ~(GPIO_MODER_MODER13);
-    GPIOE->MODER |= (GPIO_MODER_MODER13_0);
+    // Configure PE13 as output
+    GPIOE->MODER |= GPIO_MODER_MODER13_0;
 
-    // Set output type of pin 13 to push-pull output
-    GPIOE->OTYPER &= ~(GPIO_OTYPER_OT_13);
+     // Configure PE14 as output
+    GPIOE->MODER |= GPIO_MODER_MODER14_0;
 
-    // Set output speed of pin 13 to 2 MHz medium speed
-    GPIOE->OSPEEDR &= ~(GPIO_OSPEEDER_OSPEEDR13);
-    GPIOE->OSPEEDR |= (GPIO_OSPEEDER_OSPEEDR13_0);
+     // Configure PE15 as output
+    GPIOE->MODER |= GPIO_MODER_MODER15_0;
 
-    // Set pin 13 as no pull-up, no pull-down
-    GPIOE->PUPDR &= ~(GPIO_PUPDR_PUPDR13);
-
-    while(1)
-    {
-        // Toggle state of pin 13
-        GPIOE->ODR ^= (GPIO_ODR_ODR_13);
+    while(1) {
+        // Toggle PE13
+        GPIOE->ODR ^= GPIO_ODR_ODR_13;
+        GPIOE->ODR ^= GPIO_ODR_ODR_14;
+        GPIOE->ODR ^= GPIO_ODR_ODR_15;
 
         // Simple delay
-        for (uint32_t i=0; i<1000000; i++);
+        for (volatile uint32_t i = 0; i < 8000000; i++);
     }
 }
